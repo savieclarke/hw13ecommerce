17b0a6b9f0ad7e9e234cc09d9dc098d32ca43398
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2022-03-20 02:13:13 -0400

    made models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,25 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        product: 'id',
+    
+    },
+    tag_id:  {    
+      type: DataTypes.INTEGER,
+      references: {
+        product: 'id',
+      }
+    }
+    }
   },
   {
     sequelize,
