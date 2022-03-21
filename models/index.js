fe2231f471a8285e1e64de30e5566e711be84876
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2022-03-21 15:57:08 -0400

    .

--- models/index.js
+++ models/index.js
@@ -4,6 +4,9 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+init();
+
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
